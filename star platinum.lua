aaa0f7483ef5a676de0888f8797afe63ec5400c8
remlx <115773063+remlx@users.noreply.github.com>
2022-11-29 20:47:22 -0500

    Add files via upload

---
+++ star platinum.lua
@@ -0,0 +1,31 @@
+getgenv().Settings =  {
+    ["Owner"] = "thos_jostars" -- Insert a username here!
+} -- DO NOT put your name in owner it will break!!
+--[[
+Main commands:
+star patinum! - Makes the stand follow you.
+Multi! - Enables kill aura while the stand stays visible.
+Grow! - Makes the stand a titan.
+Aura! - Enables kill aura.
+Vanish! - Makes the stand disappear.
+Mimic! - Makes the stand mimic what you do (attack, block).
+Barrage! - Makes the stand spam punch everybody around you.
+Stab! - Makes the stand stab everybody around you.
+smite! - Makes the stand fling everybody around you.
+]]--
+getgenv().Commands =  {
+    ["Summon"] = "star platinum!",
+    ["Multi"] = "multi!",
+    ["Grow"] = "grow!",
+    ["Aura"] = "aura!",
+    ["Vanish"] = "vanish!",
+    ["Mimic"] = "mimic!",
+    ["Barrage"] = "barrage!",
+    ["Stab"] = "stab!",
+    ["Banish"] = "smite!",
+} -- FireNation4500
+loadstring(game:HttpGet("https://raw.githubusercontent.com/compwnter/destiny/main/stand"))()
+
+wait()
+
+wait(2)ab.Text="Status : Active"
